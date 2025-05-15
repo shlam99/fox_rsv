63ca548f93d36ba65bbba34f51209c150fd81a91
Shing Hei Lam <shlam99@hku.hk>
2025-05-15 21:25:01 +0800

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change sample prefix, reference, parameters!! Shared configuration with filename pattern
+SAMPLE_PREFIX="b8140349-847c-4e49-a1cc-18f7e4f8d2be_SQK-NBD114-24"
+REFERENCE_GENOME="RSV_refs.fasta"
+THREADS=4
+MIN_QUALITY=7
+MIN_LENGTH=500
+MAX_LENGTH=50000
+TARGET_BASES=200000000
+KEEP_PERCENT=90
