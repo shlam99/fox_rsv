e214a5cb9025eb9b6e37d5061fac6d68c5ed2bb4
Shing Hei Lam <shlam99@hku.hk>
2025-05-27 12:16:52 +0800

    Update and rename config.sh to config_empty.sh
    Signed-off-by: Shing Hei Lam <shlam99@hku.hk>

---
+++ config_empty.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Sample Prefix
+SAMPLE_PREFIX=""
+
+# Batch Name
+BATCH=""
+
+#!/bin/bash
+
+# Sample Prefix
+SAMPLE_PREFIX=""
+
+# Batch Name
+BATCH=""
+
+# Sample ID mapping (case statement)
+get_sample_id() {
+    local barcode_id="$1"
+    case "$barcode_id" in
+        "barcode01") echo "PMT" ;;
+        "barcode02") echo "PMT" ;;
+        "barcode03") echo "PMT" ;;
+        "barcode04") echo "PMT" ;;
+        "barcode05") echo "PMT" ;;
+        "barcode06") echo "PMT" ;;
+        "barcode07") echo "PMT" ;;
+        "barcode08") echo "PMT" ;;
+        "barcode09") echo "PMT" ;;
+        "barcode10") echo "PMT" ;;
+        "barcode11") echo "PMT" ;;
+        "barcode12") echo "PMT" ;;
+        "barcode13") echo "PMT" ;;
+        "barcode14") echo "PMT" ;;
+        "barcode15") echo "PMT" ;;
+        "barcode16") echo "PMT" ;;
+        "barcode17") echo "PMT" ;;
+        "barcode18") echo "PMT" ;;
+        "barcode19") echo "PMT" ;;
+        "barcode20") echo "PMT" ;;
+        "barcode21") echo "PMT" ;;
+        "barcode22") echo "PMT" ;;
+        *) echo "$barcode_id" ;;  # Fallback to barcode ID if no match
+    esac
+}
+
+# Reference Genome
+REFERENCE_GENOME=""
+
+# Filtering Parameters
+THREADS=$(nproc --all)
+MIN_QUALITY=7
+MIN_LENGTH=500
+MAX_LENGTH=50000
+TARGET_BASES=100000000
+KEEP_PERCENT=90
+
+
+# Reference Genome
+REFERENCE_GENOME=""
+
+# Filtering Parameters
+THREADS=$(nproc --all)
+MIN_QUALITY=7
+MIN_LENGTH=500
+MAX_LENGTH=50000
+TARGET_BASES=100000000
+KEEP_PERCENT=90
